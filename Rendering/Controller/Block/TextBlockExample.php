2d073df16257853d7768d33915788976a33e8c03
Vinai Kopp <vinai@netzarbeiter.com>
2021-01-28 17:59:56 +0100

    Add module Training_Rendering

---
+++ Rendering/Controller/Block/TextBlockExample.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Training\Rendering\Controller\Block;
+
+use Magento\Framework\App\ActionInterface;
+use Magento\Framework\Controller\Result\RawFactory;
+use Magento\Framework\View\LayoutInterface;
+use Magento\Framework\View\Element\Text as TextBlock;
+
+class TextBlockExample implements ActionInterface
+{
+    private $rawFactory;
+    private $layout;
+
+    public function __construct(RawFactory $rawFactory, LayoutInterface $layout)
+    {
+        $this->rawFactory = $rawFactory;
+        $this->layout = $layout;
+    }
+
+    public function execute()
+    {
+        /** @var TextBlock $block */
+        $block = $this->layout->createBlock(TextBlock::class);
+        $block->setText('This is the output of the text block now.');
+        $output = $block->toHtml();
+
+        $rawResult = $this->rawFactory->create();
+        $rawResult->setContents($output);
+        $rawResult->setHeader('Content-Type', 'text/plain', true);
+
+        return $rawResult;
+    }
+}
---
+++ Rendering/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Training_Rendering',
+    __DIR__
+);
